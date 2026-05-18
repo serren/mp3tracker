97cd325bd253287778005f93ff6eaafa9954ca0e
Sergei_Meranovich <sergei_meranovich@epam.com>
2026-05-18 10:54:08 +0300

    Fixed typo.

--- start_services.bat
+++ start_services.bat
@@ -1,7 +1,7 @@
-rem Start databases                                                                                                                     Context
-docker compose up -d resource-db song-db                                                                                              24,877 tokens
-                                                                                                                                           12% used
-rem Build and run resource-service (map external port to container port)                                                                $0.35 spent
+rem Start databases
+docker compose up -d resource-db song-db
+
+rem Build and run resource-service (map external port to container port)
 docker build -t resource-service ./resource-service
 docker run --name=resource-service --network mp3tracker_default -p 8081:8081 -e SPRING_PROFILES_ACTIVE=docker -d resource-service
 
